f4e6a92dadbcd162f4af14f376853c93cc16557d
sunnysetia <sunnysetia93@gmail.com>
2017-07-22 19:46:26 +0530

    added functions in DLL

--- LinkedListPrograms/src/doublyLL/DoublyLinkedList.java
+++ LinkedListPrograms/src/doublyLL/DoublyLinkedList.java
@@ -35,6 +35,54 @@ public class DoublyLinkedList {
 		}
 	}
 	
+	public void remove(Node curr)
+	{
+		if(curr==head)
+		{
+			head=head.next;
+			head.prev=null;
+		}
+		else if(curr==tail)
+		{
+			tail=tail.prev;
+			tail.next=null;
+		}
+		else
+		{
+			curr.prev.next=curr.next;
+			curr.next.prev=curr.prev;
+			curr.next=null;
+			curr.prev=null;
+		}
+	}
+	
+	public Node search(int k,String string)
+	{
+		Node thead = head;
+		
+		if(thead.key==k && thead.value.equals(string))
+		{
+			return thead;
+		}
+		else if(tail.key==k && tail.value.equals(string))
+		{
+			return tail;
+		}
+		else
+		{
+			while(thead!=null)
+			{
+				if(thead.key==k && thead.value.equals(string))
+				{
+					return thead;
+				}
+				thead=thead.next;
+			}
+		}
+		
+		return null;
+	}
+	
 	public void display()
 	{
 		Node thead=head;
@@ -57,6 +105,10 @@ public class DoublyLinkedList {
 		dll.addAtHead(1, "page1");
 		dll.addAtHead(2, "page2");
 		dll.addAtTail(3, "page3");
+		dll.display();
+		Node x = dll.search(2, "page2");
+		System.out.println("searched node->" + x.key);
+		dll.remove(x);
 		dll.display();
 
 	}
