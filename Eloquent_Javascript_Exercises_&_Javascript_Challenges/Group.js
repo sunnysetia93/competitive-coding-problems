f2bdce09c3f77bdce23054de268fdf3f492cf315
sunny setia <sunnysetia93@gmail.com>
2019-05-24 02:57:52 +0530

    group update

--- Eloquent_Javascript_Exercises_&_Javascript_Challenges/Group.js
+++ Eloquent_Javascript_Exercises_&_Javascript_Challenges/Group.js
@@ -1,3 +1,9 @@
+// Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
+
+// Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
+
+// Give the class a static from method that takes an iterable object as argument and creates a group that contains all the values produced by iterating over it.
+
 class Group{
     constructor(){
         this.set = [];
