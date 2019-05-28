9ae6f1b962555f5b25e12719a41c999716618cf8
sunny setia <sunnysetia93@gmail.com>
2019-05-29 00:28:42 +0530

    pairs problem

---
+++ HackerRank_Problems/ProblemSolving/Pairs.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int count=0;
+        for(int value:arr)
+            map.put(value,Math.abs(value-k));
+
+        for(int value:arr){
+
+            int calculatedValue = map.get(value);
+            if(calculatedValue!=value && map.containsKey(calculatedValue))
+                count++;
+
+        }
+
+        System.out.println(map);
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
