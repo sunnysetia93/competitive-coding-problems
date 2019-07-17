ffe1347c49225643affae1d4566ff78338b8d1ac
sunny setia <sunnysetia93@gmail.com>
2019-07-18 02:07:24 +0530

    min element in stack using constant space

---
+++ Stacks & Queues/MinElementInStackConstantSpace.java
@@ -0,0 +1,69 @@
+// https://www.geeksforgeeks.org/design-a-stack-that-supports-getmin-in-o1-time-and-o1-extra-space/
+
+import java.util.*;
+
+class StackMin {
+    int currentMin;
+    private Stack<Integer> stack = new Stack<Integer>();
+
+    public void push(int value){
+        if(stack.isEmpty()){
+            stack.push(value);
+            currentMin=value;
+        }
+        else{
+            if(value>=currentMin){
+                stack.push(value);
+            }
+            else{
+                stack.push(2*value - currentMin);
+                currentMin = value;
+            }
+        }
+    }
+
+    public int pop(){
+        if(stack.isEmpty())
+            throw new EmptyStackException();
+
+        int popped = stack.pop();
+        if(popped<currentMin){
+            int min = currentMin;
+            currentMin = 2*currentMin - popped;
+            return min;
+        }
+
+        return popped;
+    }
+
+    public int peek(){
+        if(stack.isEmpty())
+            throw new EmptyStackException();
+        
+        int popped = stack.peek();
+        if(popped<currentMin)
+            return currentMin;
+        return popped;
+    }
+
+    public int getMin(){
+        if(stack.isEmpty())
+            throw new EmptyStackException();
+        
+        return currentMin;
+    }
+
+    public static void main(String[] args){
+        StackMin s = new StackMin();
+        s.push(3); 
+        s.push(5); 
+        System.out.println(s.getMin()); 
+        s.push(2); 
+        s.push(1); 
+        System.out.println(s.getMin()); 
+        System.out.println(s.pop()); 
+        System.out.println(s.getMin()); 
+        System.out.println(s.pop()); 
+        System.out.println(s.peek()); 
+    }
+}
