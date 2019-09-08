3d3a0f5064738580340f384cf0f8bcf5bf98f427
sunny setia <sunnysetia93@gmail.com>
2019-09-08 16:29:32 +0530

    pairs with difference update

--- JavaScript_Questions/pairsWithDifference.js
+++ JavaScript_Questions/pairsWithDifference.js
@@ -11,6 +11,22 @@ function findPairs(arr,diff){
     return pairs
 }
 
+// duplicate elements allowed in pairs
+function findPairsUsingMap(arr,diff){
+    let map = {};
+    let pairs = [];
+    for(let i of arr){
+        map[i-diff]=i
+    }
+
+    for(let i of arr){
+        if(map[i]!=null){
+            pairs.push([i,map[i]]);
+        }
+    }
+    return pairs;
+}
+
 function findPairsDP(arr,diff){
     let sortedArr = arr.sort((a,b)=>a-b);
     let arrLen = sortedArr.length;
@@ -31,9 +47,12 @@ function findPairsDP(arr,diff){
 }
 
 function main(){
-    let arr = [3, 5, 10, 15, 19, 11, 9];
-    let diff = 4;
+    // let arr = [3, 5, 10, 15, 19, 11, 9];
+    // let diff = 4;
+    let arr = [0, -1, -2, 2, 1];
+    let diff = 1;
     console.log(findPairs(arr,diff));
     console.log(findPairsDP(arr,diff));
+    console.log(findPairsUsingMap(arr,diff));
 }
 main();
