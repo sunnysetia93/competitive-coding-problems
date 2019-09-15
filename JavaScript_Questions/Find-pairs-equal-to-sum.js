68d65869e6f849a34db6ce0c317fe95e1e1b52aa
sunny setia <sunnysetia93@gmail.com>
2019-09-15 23:40:31 +0530

    update to find pairs equal to sum

--- JavaScript_Questions/Find-pairs-equal-to-sum.js
+++ JavaScript_Questions/Find-pairs-equal-to-sum.js
@@ -27,10 +27,31 @@ function hashingSolution(arr,k){
     }
     return pairs;
 }
+function twoPointers(arr,k){
+    let len = arr.length;
+    let pairs = [];
+    
+    arr = arr.sort((a,b)=>a-b);
 
+    let i=0,j=len-1;
+    while(i!=j){
+        let sum = arr[i]+arr[j];
+        if(sum==k){
+            pairs.push([arr[i],arr[j]]);
+            i++;
+            j--;
+        }
+        else if(sum<k)
+            i++;
+        else
+            j--;
+    }
+    return pairs;
+}
 (()=>{
     let arr = [5,9,12,4,23,1];
     let k = 13;
     console.log(bruteForceSolution(arr,k));
     console.log(hashingSolution(arr,k));
+    console.log(twoPointers(arr,k));
 })();
