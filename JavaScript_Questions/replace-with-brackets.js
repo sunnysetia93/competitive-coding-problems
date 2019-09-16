efc7f260c2efb2704c1e87d6966916867507897b
sunny setia <sunnysetia93@gmail.com>
2019-09-16 13:33:07 +0530

    replace with brackets

---
+++ JavaScript_Questions/replace-with-brackets.js
@@ -0,0 +1,32 @@
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
+function duplicateEncode(word){
+    let characters = word.toLowerCase().split("");
+    let map={};
+    for(let ch of characters){
+      if(map[ch]!=null){
+          let cnt = map[ch];
+          map[ch]=cnt+1;
+      }
+      else
+        map[ch]=1;
+    }
+    let result = "";
+    for(let ch of characters){
+      if(map[ch]>1)
+          result+=")"
+      else
+        result+="("
+    }
+    return result;
+}
