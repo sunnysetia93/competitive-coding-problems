650192a7f09841ab9d15fa78f6d26663fa6601eb
sunny setia <sunnysetia93@gmail.com>
2019-10-11 16:08:16 +0530

    Done - count-ip-addresses

--- Code-Wars/JS/count-ip-addresses.js
+++ Code-Wars/JS/count-ip-addresses.js
@@ -1,23 +1,7 @@
 // https://www.codewars.com/kata/count-ip-addresses/train/javascript
 
 function ipsBetween(start, end){
-    let startArr = start.split(".");
-    let endArr = end.split(".");
-    console.log(startArr,endArr);
-    let level=0;
-    let count =0;
-    for(let i=3;i>=0;i--){  
-      if(startArr[i]==0 && endArr[i]==0)
-        break;
-      let times = level*256;
-      if(level!=0)
-        count += (Math.abs(endArr[i]-startArr[i]))*times;
-      else
-        count += (Math.abs(endArr[i]-startArr[i]));
-        
-      console.log(count);
-      level++;
-    }
-    
-    return count;
-  }
+  let ip1 = start.split('.').map(n => parseInt(n)).reduce((s, b) => (s << 8) + b, 0);
+  let ip2 = end.split('.').map(n => parseInt(n)).reduce((s, b) => (s << 8) + b, 0);
+  return ip2 - ip1;
+}
