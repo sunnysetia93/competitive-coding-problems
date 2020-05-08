d2c302babd4ebb068c7d0cfcf96abb5dfdbc532b
sunny setia <sunnysetia93@gmail.com>
2020-05-09 00:00:51 +0530

    updated minStack

--- Stacks_&_Queues/MinElementInStack.java
+++ Stacks_&_Queues/MinElementInStack.java
@@ -9,52 +9,72 @@ import java.lang.*;
 import java.io.*;
 
 
-class MinStack extends Stack<Integer>
-{
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class MinStack extends Stack<Integer> {
     Stack<Integer> stack;
-    public MinStack()
-    {
+
+    public MinStack() {
         stack = new Stack<Integer>();
     }
-    
-    public void push(int val)
-    {
-        if(val<=min())
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
             stack.push(val);
+            super.push(val);
+            return;
+        }
+
+        int minVal = min();
+        if (val <= minVal) {
+            stack.push(val);
+        } else {
+            stack.push(minVal);
+        }
         super.push(val);
-        
+
     }
-    public Integer pop()
-    {
+
+    public Integer pop() {
+        if (stack.isEmpty())
+            return Integer.MAX_VALUE;
+
         int val = super.pop();
-        if(val==min())
-            stack.pop();
-        
+        stack.pop();
         return val;
     }
-    public int min()
-    {
-        if(stack.isEmpty())
+
+    public int min() {
+        if (stack.isEmpty())
             return Integer.MAX_VALUE;
-        else
-        {
+        else {
             return stack.peek();
         }
-            
+
     }
-	public static void main (String[] args)
-	{
-		MinStack minstack = new MinStack();
-		minstack.push(5);
-		minstack.push(10);
-		System.out.println(minstack.min());
-		minstack.push(25);
-		System.out.println(minstack.min());
-		minstack.push(2);
-		System.out.println(minstack.min());
-		minstack.pop();
-		System.out.println(minstack.min());
-		
-		
-	}
+
+    public static void main(String[] args) {
+        MinStack minstack = new MinStack();
+        minstack.push(5);
+        minstack.push(10);
+        System.out.println(minstack.min()); //5
+        minstack.push(25);
+        System.out.println(minstack.min()); //5
+        minstack.push(2);
+        System.out.println(minstack.min()); //2
+        minstack.pop();
+        System.out.println(minstack.min()); //5
+        minstack.pop();
+        System.out.println(minstack.min()); //5
+        minstack.pop();
+        System.out.println(minstack.min()); //5
+        minstack.pop();
+        System.out.println(minstack.min()); //2147483647 =>empty
+        minstack.pop();
+        System.out.println(minstack.min()); //2147483647 =>empty
+
+    }
 }
