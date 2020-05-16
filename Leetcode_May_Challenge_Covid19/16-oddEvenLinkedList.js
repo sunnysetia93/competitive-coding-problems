0d5283458c586f964feef37efc9988c1272c5e93
sunny setia <sunnysetia93@gmail.com>
2020-05-17 00:55:52 +0530

    april leet code sol - min path sum

--- Leetcode_May_Challenge_Covid19/16-oddEvenLinkedList.js
+++ Leetcode_May_Challenge_Covid19/16-oddEvenLinkedList.js
@@ -10,7 +10,6 @@
     Example 2:
         Input: 2->1->3->5->6->4->7->NULL
         Output: 2->3->6->7->1->5->4->NULL
-
     Note:
         The relative order inside both the even and odd groups should remain as it was in the input.
         The first node is considered odd, the second node even and so on ...
