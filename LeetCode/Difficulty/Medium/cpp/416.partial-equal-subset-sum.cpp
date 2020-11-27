9f813c4bf49a6b0e25d23f481dcb5db153b1f9e1
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2020-11-27 17:16:30 +0530

    added problem no.27 of November_LeetCoding_Challenge

---
+++ LeetCode/Difficulty/Medium/cpp/416.partial-equal-subset-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        bitset<10001> bits(1);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) return false;      // odd sum can't be divided in two equal halves
+        
+        for (int n : nums) bits |= bits << n;   // every 1 in the bitset corresponds to a possible sum
+        return bits[sum >> 1] == 1;     // check if sum/2 is possible
+    }
+};
