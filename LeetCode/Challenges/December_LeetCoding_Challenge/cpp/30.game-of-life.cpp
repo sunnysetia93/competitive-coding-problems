5146e586e1f92439c541f072f3ed79bd62362c77
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2020-12-30 14:21:08 +0530

    added problem no.30 of December_LeetCoding_Challenge

---
+++ LeetCode/Challenges/December_LeetCoding_Challenge/cpp/30.game-of-life.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        vector<vector<int>> dirn = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1},
+                                    {1, 1}, {1, 0}, {1, -1}, {0, -1}};
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                int nAliveNeighbours = 0;
+                for (auto& d : dirn) {
+                    int rr = i + d[0];
+                    int cc = j + d[1];
+                    if (rr < 0 || rr >= m || cc < 0 || cc >= n) continue;
+                    
+                    nAliveNeighbours += board[rr][cc] & 1;
+                }
+                if (nAliveNeighbours == 3 ||
+                    (nAliveNeighbours == 2 && board[i][j]))
+                    board[i][j] |= 2;
+            }
+        }
+
+        for (int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j)
+                board[i][j] >>= 1;
+    }
+};
