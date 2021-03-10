abfb6daea346a5701a30f7776edb33a14b227715
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2021-03-10 20:18:20 +0530

    added problem no.9 of March_LeetCoding_Challenge

---
+++ LeetCode/Challenges/2021/March_LeetCoding_Challenge/cpp/9.add-one-row-to-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int v, int d) {
+        if (!root) return NULL;
+        if (d == 1) {
+            return new TreeNode(v, root, NULL);
+        } else if (d == 2) {
+            root->left = new TreeNode(v, root->left, NULL);
+            root->right = new TreeNode(v, NULL, root->right);
+        } else {
+            addOneRow(root->left, v, d-1);
+            addOneRow(root->right, v, d-1);
+        }
+        return root;
+    }
+};
