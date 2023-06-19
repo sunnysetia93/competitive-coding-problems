a505a5370cf7853e2d89d22b22af759fd6a93da7
sunny setia <sunnysetia93@gmail.com>
2023-06-19 16:53:49 +0530

    update

--- JavaScriptQuestions/trimArray.js
+++ JavaScriptQuestions/trimArray.js
@@ -1,4 +1,4 @@
-const trimArray = (arr, n, m) => arr.splice(0,arr.length - m).splice(n);
+const trimArray = (arr, n, m) => arr.slice(n,arr.length - m);
 
 
 console.log(trimArray([1, 2, 3, 4, 5, 6], 2, 1));
