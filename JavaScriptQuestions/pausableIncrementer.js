435c68b1351a6b1e035b72b333bdba8006b11bc4
sunny setia <sunnysetia93@gmail.com>
2023-07-04 14:16:10 +0530

    pausable incrementer

--- JavaScriptQuestions/pausableIncrementer.js
+++ JavaScriptQuestions/pausableIncrementer.js
@@ -1,30 +1,57 @@
-class Timer {
-  constructor(initialValue, step) {
-    this.intervalId = null;
-    this.count = initialValue;
-    this.step = step;
-  }
+// class Timer {
+//   constructor(initialValue, step) {
+//     this.intervalId = null;
+//     this.count = initialValue;
+//     this.step = step;
+//   }
+//
+//   startTimer() {
+//     if (this.intervalId !== null)
+//       return;
+//     this.intervalId = setInterval(() => {
+//       console.log(this.count);
+//       this.count += this.step;
+//     }, 1000)
+//   }
+//
+//   stopTimer() {
+//     clearInterval(this.intervalId);
+//     this.intervalId = null;
+//   }
+// }
 
-  startTimer() {
-    if (this.intervalId !== null)
+// using closures - makes the internal variables private
+function timerFunction(init, step = 1) {
+  let intervalId;
+  let count = init;
+
+  const startTimer = () => {
+    if (intervalId != null)
       return;
-    this.intervalId = setInterval(() => {
-      console.log(this.count);
-      this.count += this.step;
+    intervalId = setInterval(() => {
+      console.log(count);
+      count += step;
     }, 1000)
   }
 
-  stopTimer() {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
+  const stopTimer = () => {
+    clearInterval(intervalId);
+    intervalId = null;
   }
+
+  return {
+    startTimer,
+    stopTimer
+  }
+
 }
 
-const timer = new Timer(10, 10);
-timer.startTimer();
-setTimeout(() => timer.stopTimer(), 5000);
-setTimeout(() => timer.startTimer(), 8000);
-setTimeout(() => timer.stopTimer(), 12000);
+
+// const timer = new Timer(10, 10);
+// timer.startTimer();
+// setTimeout(() => timer.stopTimer(), 5000);
+// setTimeout(() => timer.startTimer(), 8000);
+// setTimeout(() => timer.stopTimer(), 12000);
 /**
  10
  20
@@ -37,3 +64,9 @@ setTimeout(() => timer.stopTimer(), 12000);
  70
  stop
  **/
+
+const timer = timerFunction(10, 10);
+timer.startTimer();
+setTimeout(() => timer.stopTimer(), 5000);
+setTimeout(() => timer.startTimer(), 8000);
+setTimeout(() => timer.stopTimer(), 12000);
