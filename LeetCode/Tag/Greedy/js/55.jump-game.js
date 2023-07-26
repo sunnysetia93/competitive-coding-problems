9364e3e86fd7a14c0b6db37823f99d263d4debfb
sunny setia <sunnysetia93@gmail.com>
2023-07-26 18:35:37 +0530

    55 lc

---
+++ LeetCode/Tag/Greedy/js/55.jump-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let goal = nums.length - 1;
+
+    for(let i = nums.length -1; i >= 0; i--){
+        if(i + nums[i] >= goal){
+            goal = i;
+        }
+    }
+
+    return goal === 0 ? true : false;
+};
